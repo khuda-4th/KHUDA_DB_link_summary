c5b78937408aec5ae036f753296e7db7ddbfdd3b
임소영 <soyeong020420@gmail.com>
2023-11-29 20:02:10 +0900

    streamlit 수정

--- link.py
+++ link.py
@@ -2,25 +2,67 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-st.title('LINK')
-st.caption('Link Is Not Kind')
+json_data = [
+    {'title' : '제목입니다.',
+    'summary' : '이렇궁저렇궁이렇궁저렇궁',
+    'keyword' : ['CS', 'Khuda', '이렇궁'],
+    'url' : 'https://velog.io/@so_yeong',
+    'img' : 'img_link'
+    },
 
-st.sidebar.title('Tags')
-tag_list = ['CS', '머신러닝', 'AI', 'KHUDA']
+   {
+    'title' : '제목입니다.1',
+    'summary' : '이렇궁저렇궁이렇궁저렇궁1',
+    'keyword' : ['CS1', 'Khuda1', '머신러닝'],
+    'url' : 'https://velog.io/@so_yeong',
+    'img' : 'img_link'
+    }, 
 
-article = {'title' : ['cs지식과 khuda'],
-           'summary' : ['이렇궁저렇궁'],
-           'keyword' : [['CS', 'KHUDA', '머신러닝']]}
-
+   {
+    'title' : '제목입니다.1',
+    'summary' : '이렇궁저렇궁이렇궁저렇궁1',
+    'keyword' : ['AI', 'Khuda1', '이렇궁1'],
+    'url' : 'https://velog.io/@so_yeong',
+    'img' : 'img_link'
+    }, 
+   {
+    'title' : '제목입니다.1',
+    'summary' : '이렇궁저렇궁이렇궁저렇궁1',
+    'keyword' : ['AI', 'Khuda1', '이렇궁1'],
+    'url' : 'https://velog.io/@so_yeong',
+    'img' : 'img_link'
+    }, 
+   {
+    'title' : '제목입니다.1',
+    'summary' : '이렇궁저렇궁이렇궁저렇궁1',
+    'keyword' : ['AI', 'Khuda1', '이렇궁1'],
+    'url' : 'https://velog.io/@so_yeong',
+    'img' : 'img_link'
+    }, 
+   {
+    'title' : '제목입니다.1',
+    'summary' : '이렇궁저렇궁이렇궁저렇궁1',
+    'keyword' : ['AI', 'Khuda1', '이렇궁1'],
+    'url' : 'https://velog.io/@so_yeong',
+    'img' : 'img_link'
+    }, 
+   {
+    'title' : '제목입니다.1',
+    'summary' : '이렇궁저렇궁이렇궁저렇궁1',
+    'keyword' : ['AI', 'Khuda1', '이렇궁1'],
+    'url' : 'https://velog.io/@so_yeong',
+    'img' : 'img_link'
+    }, 
+   {
+    'title' : '제목입니다.1',
+    'summary' : '이렇궁저렇궁이렇궁저렇궁1',
+    'keyword' : ['AI', 'Khuda1', '이렇궁1'],
+    'url' : 'https://velog.io/@so_yeong',
+    'img' : 'img_link'
+    }, 
+]
 url = 'https://velog.io/@so_yeong'
-
-for tag in tag_list:
-    globals()["tag_{}".format(tag)] = st.sidebar.checkbox(tag)
-
-for tag in tag_list:
-    if globals()["tag_{}".format(tag)] :
-        st.sidebar.write('{}'.format(tag))
-
+df = pd.DataFrame(json_data)
 
 def truncate_text(text, max_length = 50):
     if len(text) <= max_length:
@@ -28,34 +70,60 @@ def truncate_text(text, max_length = 50):
     else:
         return text[:max_length-3] + "..."
 
+st.title('LINK')
+st.caption('Link Is Not Kind')
+# 'keyword' 필드의 요소들을 중복 없이 추출
+tag_list = list({keyword for entry in json_data for keyword in entry['keyword']})
+select = st.multiselect(
+    'Select Keywords',
+    tag_list)
 
-# with st.container():
-#    st.write("This is inside the container")
-#    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-col1, col2, col3 = st.columns(3)
-
-
-with col1:
-   with st.container():
-    st.image("img/test_img.png", width=220, use_column_width=100)
-    st.write('**[titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle](%s)**' %url)
-    st.write(truncate_text('summarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummary'))
-
-   with st.container():
-    st.image("img/test_img.png", width=220, use_column_width=100)
-    st.write('**[title](%s)**' %url)
-    st.write('summarysummarysummarysummarysummarysummary')
+if select:
+    filtered_df = df[df['keyword'].apply(lambda x: any(keyword in x for keyword in select))]
+    url_cnt = len(filtered_df)
+    col1, col2, col3 = st.columns(3)
 
 
-with col2:
-   with st.container():
-    st.image("img/test_img.png", width=220, use_column_width=100)
-    st.write('**[title](%s)**' %url)
-    st.write('summarysummarysummarysummarysummarysummary')
+    with col1:
+        for i in range(0, url_cnt, 3):
+            with st.container():
+                st.image("img/test_img.png", width=220, use_column_width=100)
+                st.write(f'**[{filtered_df["title"].iloc[i]}]({url})**')
+                st.write(truncate_text(filtered_df['summary'].iloc[i]))
 
-with col3:
-   with st.container():
-    st.image("img/test_img.png", width=220, use_column_width=100)
-    st.write('**[title](%s)**' %url)
-    st.write('summarysummarysummarysummarysummarysummary')
+    with col2:
+        for i in range(1, url_cnt, 3):
+            with st.container():
+                st.image("img/test_img.png", width=220, use_column_width=100)
+                st.write(f'**[{filtered_df["title"].iloc[i]}]({url})**')
+                st.write(truncate_text(filtered_df['summary'].iloc[i]))
+
+    with col3:
+        for i in range(2, url_cnt, 3):
+            with st.container():
+                st.image("img/test_img.png", width=220, use_column_width=100)
+                st.write(f'**[{filtered_df["title"].iloc[i]}]({url})**')
+                st.write(truncate_text(filtered_df['summary'].iloc[i]))
+
+else:
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        for i in range(0, len(df), 3):
+            with st.container():
+                st.image("img/test_img.png", width=220, use_column_width=100)
+                st.write(f'**[{df["title"].iloc[i]}]({url})**')
+                st.write(truncate_text(df['summary'].iloc[i]))
+
+    with col2:
+        for i in range(1, len(df), 3):
+            with st.container():
+                st.image("img/test_img.png", width=220, use_column_width=100)
+                st.write(f'**[{df["title"].iloc[i]}]({url})**')
+                st.write(truncate_text(df['summary'].iloc[i]))
+
+    with col3:
+        for i in range(2, len(df), 3):
+            with st.container():
+                st.image("img/test_img.png", width=220, use_column_width=100)
+                st.write(f'**[{df["title"].iloc[i]}]({url})**')
+                st.write(truncate_text(df['summary'].iloc[i]))
